cf90e6e0943723f7fa5abf915790ff0fdd0ddeb9
sunpra <sunpra12@gmail.com>
2024-03-04 10:53:03 +0545

    Email validation added.

--- app/src/main/java/com/sunpra/memories/LoginScreenViewModel.kt
+++ app/src/main/java/com/sunpra/memories/LoginScreenViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import java.util.regex.Pattern
 
 class LoginScreenViewModel: ViewModel() {
     private val _uiState = MutableStateFlow<LoginScreenUiState>(LoginScreenUiState())
@@ -32,11 +33,37 @@ class LoginScreenViewModel: ViewModel() {
             currentState.copy(rememberMe = !currentState.rememberMe)
         }
     }
+
+    fun onLoginClicked(){
+        if(validate()){
+
+        }
+    }
+
+    fun validate() : Boolean{
+        var isValid = true
+
+        val email = uiState.value.email
+        if(email.isEmpty()){
+            isValid = false
+            _uiState.update { currentState ->
+                currentState.copy(emailError =  "Email is required.")
+            }
+        }else if(!Pattern.matches("^(.+)@(\\\\S+)\$", email)){
+            isValid = false
+            _uiState.update { currentState ->
+                currentState.copy(emailError =  "Invalid email address.")
+            }
+        }
+
+        return isValid
+    }
 }
 
 data class LoginScreenUiState(
     val email: String = "",
     val password: String = "",
     val rememberMe: Boolean = false,
-    val isPasswordVisible: Boolean = false
+    val isPasswordVisible: Boolean = false,
+    val emailError: String? = null
 )
--- app/src/main/java/com/sunpra/memories/MainActivity.kt
+++ app/src/main/java/com/sunpra/memories/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sunpra.memories
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -24,9 +25,6 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -55,9 +53,10 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreen() {
-    val viewModel =
+fun LoginScreen(
+    viewModel: LoginScreenViewModel =
         ViewModelProvider(LocalContext.current as MainActivity)[LoginScreenViewModel::class.java]
+) {
 
     val uiState by viewModel.uiState.collectAsState()
 
@@ -95,6 +94,13 @@ fun LoginScreen() {
                 value = uiState.email,
                 onValueChange = viewModel::onEmailChanged,
             )
+            AnimatedVisibility(visible = uiState.emailError != null) {
+                Text(
+                    modifier = Modifier.padding(start = 4.dp, top = 2.dp),
+                    text = uiState.emailError ?: "",
+                    style = MaterialTheme.typography.labelMedium.copy(color = MaterialTheme.colorScheme.error)
+                )
+            }
             TextField(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -132,7 +138,8 @@ fun LoginScreen() {
                     .padding(top = 16.dp)
                     .fillMaxWidth()
                     .heightIn(min = 52.dp),
-                onClick = { }) {
+                onClick = viewModel::onLoginClicked
+            ) {
                 Text(text = "LOGIN")
             }
 
@@ -140,7 +147,10 @@ fun LoginScreen() {
                 modifier = Modifier.padding(top = 10.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Checkbox(checked = uiState.rememberMe, onCheckedChange = viewModel::rememberMeToggled)
+                Checkbox(
+                    checked = uiState.rememberMe,
+                    onCheckedChange = viewModel::rememberMeToggled
+                )
                 Text(text = "Remember me.")
             }
 
