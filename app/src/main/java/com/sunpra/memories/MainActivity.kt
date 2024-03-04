b3e9738ab34a2ad42d8dd859ade68c7bed3126c8
sunpra <sunpra12@gmail.com>
2024-03-04 10:01:27 +0545

    Login Screen State handled.

--- app/src/main/java/com/sunpra/memories/MainActivity.kt
+++ app/src/main/java/com/sunpra/memories/MainActivity.kt
@@ -3,11 +3,15 @@ package com.sunpra.memories
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,11 +23,17 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,6 +54,30 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen() {
+    var email: String by remember {
+        mutableStateOf("")
+    }
+    var password: String by remember {
+        mutableStateOf("")
+    }
+    var rememberMe: Boolean by remember {
+        mutableStateOf(false)
+    }
+    var isPasswordVisible: Boolean by remember {
+        mutableStateOf(false)
+    }
+    val onEmailChanged: (String) -> Unit = {
+        email = it
+    }
+    val onPasswordChanged: (String) -> Unit = {
+        password = it
+    }
+    val rememberMeToggled: (Boolean) -> Unit = {
+        rememberMe = it
+    }
+    val onPasswordToggled: () -> Unit = {
+        isPasswordVisible = !isPasswordVisible
+    }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -63,6 +97,7 @@ fun LoginScreen() {
             modifier = Modifier
                 .padding(padding)
                 .padding(16.dp)
+                .verticalScroll(rememberScrollState())
         ) {
             Text(text = "WELCOME", style = MaterialTheme.typography.headlineMedium)
             Text(
@@ -74,22 +109,33 @@ fun LoginScreen() {
                     .fillMaxWidth()
                     .padding(top = 32.dp),
                 label = { Text(text = "Email") },
-                value = "sunpra12@gmail.com",
-                onValueChange = {},
+                value = email,
+                onValueChange = onEmailChanged,
             )
             TextField(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 16.dp),
-                value = "password",
+                visualTransformation = if (isPasswordVisible) VisualTransformation.None
+                else PasswordVisualTransformation(),
+                value = password,
                 label = { Text(text = "Password") },
                 trailingIcon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.eye),
-                        contentDescription = "Password view icon."
-                    )
+                    if (isPasswordVisible) {
+                        Icon(
+                            modifier = Modifier.clickable(enabled = true, onClick = onPasswordToggled),
+                            painter = painterResource(id = R.drawable.eye),
+                            contentDescription = "Password view icon."
+                        )
+                    } else {
+                        Icon(
+                            modifier = Modifier.clickable(enabled = true, onClick = onPasswordToggled),
+                            painter = painterResource(id = R.drawable.eye_off),
+                            contentDescription = "Password view icon."
+                        )
+                    }
                 },
-                onValueChange = {},
+                onValueChange = onPasswordChanged,
             )
 
             Button(
@@ -105,7 +151,7 @@ fun LoginScreen() {
                 modifier = Modifier.padding(top = 10.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Checkbox(checked = true, onCheckedChange = {})
+                Checkbox(checked = rememberMe, onCheckedChange = rememberMeToggled)
                 Text(text = "Remember me.")
             }
 
